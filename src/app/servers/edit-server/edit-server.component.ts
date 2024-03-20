517015fe4b4fad15f6281e9b20555183640e1d60
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-20 17:19:34 +0530

    Marking use of deactivateGuard to alert user on abruptly moving away.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) {}
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -24,8 +31,12 @@ export class EditServerComponent implements OnInit {
       this.allowEdit = queryParams.allowEdit === '1' ? true : false;
     });
     this.route.fragment.subscribe();
-
-    this.server = this.serversService.getServer(1);
+    const id = this.route.snapshot.params.id;
+    this.server = this.serversService.getServer(id);
+    // subscribe to route params to update the id if params change
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params.id);
+    });
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -35,5 +46,22 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if (
+      (this.serverName != this.server.name || this.serverStatus != this.server.status) &&
+      !this.changesSaved
+    ) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
   }
 }
